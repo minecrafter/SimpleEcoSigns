770636e6bad4cda778914365d105889fafabc8cb
Aaron Abbott <aaron.abbott1@gmail.com>
2013-06-16 22:14:47 -0400

    Rearranged and made a listener class. Added support for abbreviations.
    Big commit. I moved the listener into their own class, made a few of the methods static that weren't before.
    I also added support for abbreviations. If the player types "thin" and the only item beginning with thin is "thinglass", then the sign will give thinglass. However, if there is more than one match, for instance, if the player type "boots", the sign is invalid.

--- src/com/tropicalwikis/tuxcraft/plugins/simpleecosigns/SimpleEcoSigns.java
+++ src/com/tropicalwikis/tuxcraft/plugins/simpleecosigns/SimpleEcoSigns.java
@@ -38,21 +38,14 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.block.SignChangeEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleEcoSigns extends JavaPlugin implements Listener {
-	private Economy econ;
-	private HashMap<String, String> namesToIds = new HashMap<String, String>();
+	private static Economy econ;
+	private static HashMap<String, String> namesToIds = new HashMap<String, String>();
 	@Override
 	public void onEnable() {
 		if (getServer().getPluginManager().getPlugin("Vault") != null) {
@@ -65,7 +58,8 @@ public class SimpleEcoSigns extends JavaPlugin implements Listener {
 			getServer().getPluginManager().disablePlugin(this);
 			return;
 		}
-		getServer().getPluginManager().registerEvents(this, this);
+		
+		getServer().getPluginManager().registerEvents(new SignListeners(), this);
 		try {
 			File pluginFolder = this.getDataFolder();
 			if (!pluginFolder.exists()) {
@@ -87,95 +81,43 @@ public class SimpleEcoSigns extends JavaPlugin implements Listener {
 		}
 	}
 	
-    @EventHandler
-    public void signChange(SignChangeEvent event) {
-        if (event.getLine(0).equalsIgnoreCase("[Buy]") || event.getLine(0).equalsIgnoreCase("[Sell]")) {
-            // Verify the sign:
-        	// [Buy]
-        	// 1
-        	// Diamond
-        	// $5000
-        	try {
-        		Integer.valueOf(event.getLine(1));
-        		String s2 = event.getLine(2);
-        		String s3 = s2;
-        		if(s2.contains(":")) {
-        			s3 = s2.split(":")[0];
-        		}
-        		try {
-        			Integer.valueOf(s3);
-        		} catch (NumberFormatException e) {
-        			if(!namesToIds.containsKey(s3.toLowerCase()))
-        				if(Material.matchMaterial(s3) == null)
-        					throw new Exception();
-        		}
-        		Double.valueOf(event.getLine(3).replace("$", ""));
-        	} catch (Exception e) {
-        		event.setLine(0, ChatColor.DARK_RED + event.getLine(0));
-        		event.getPlayer().sendMessage(ChatColor.RED + "Sign format invalid");
-        		return;
-        	}
-            if (event.getPlayer().hasPermission("simpleecosigns.create")) {
-                event.setLine(0, ChatColor.DARK_BLUE + event.getLine(0));
-            } else {
-                event.setLine(0, ChatColor.DARK_RED + event.getLine(0));
-            }
-        }
-    }
-    
-    @SuppressWarnings("deprecation")
-	@EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Player p = event.getPlayer();
-        if (event.hasBlock() && event.getClickedBlock().getState() instanceof Sign && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            Sign s = (Sign) event.getClickedBlock().getState();
-            if(this.blockIsValidEcoSign(s)) {
-            	if(s.getLine(0).matches(ChatColor.DARK_BLUE + "(?i)\\[Buy\\]")) {
-            		if(econ.getBalance(p.getName()) >= Double.valueOf(s.getLine(3).replace("$", ""))) {
-            			econ.withdrawPlayer(p.getName(), Double.valueOf(s.getLine(3).replace("$", "")));
-            			if(p.getInventory().firstEmpty() == -1) {
-            				p.sendMessage(ChatColor.RED + "You do not have enough inventory space. The items will be dropped on the ground.");
-            				p.getWorld().dropItemNaturally(p.getLocation(), nameToItemStack(s.getLine(2), Integer.valueOf(s.getLine(1))));
-            			} else {
-            				p.getInventory().addItem(nameToItemStack(s.getLine(2), Integer.valueOf(s.getLine(1))));
-            			}
-            			p.sendMessage(ChatColor.GREEN + "Item purchased.");
-            		} else {
-            			p.sendMessage(ChatColor.RED + "You do not have enough funds.");
-            		}
-            		p.updateInventory();
-            	}
-            	if(s.getLine(0).matches(ChatColor.DARK_BLUE + "(?i)\\[Sell\\]")) {
-            		// I have to do this backwards-ass way because Bukkit sucks.
-            		Integer amt = Integer.valueOf(s.getLine(1));
-            		Integer s2 = 0;
-            		ItemStack i = nameToItemStack(s.getLine(2), amt);
-            		if(!p.getInventory().containsAtLeast(i, amt)) {
-            			p.sendMessage(ChatColor.RED + "You don't have enough items in your inventory.");
-            			return;
-            		}
-            		for(ItemStack item : p.getInventory().getContents()) {
-            			if(item != null && item.isSimilar(i) && s2 < amt) {
-            				p.getInventory().removeItem(item);
-            				s2 = s2 + item.getAmount();
-            			}
-            		}
-            		p.updateInventory();
-            		if(s2 > amt) {
-            			s2 = s2 - amt;
-            			p.getInventory().addItem(new ItemStack(i.getTypeId(), s2, i.getDurability()));
-            			p.updateInventory();
-            		}
-        			econ.depositPlayer(p.getName(), Double.valueOf(s.getLine(3).replace("$", "")));
-        			p.sendMessage(ChatColor.GREEN + "Item sold.");
-        			p.updateInventory();
-        			return;
-            	}
-            }
-        }
-    }
-    
-    private ItemStack nameToItemStack(String name, Integer amt) {
+	@Override
+	public void onDisable() {
+		
+	}
+	
+    //some methods to access econ
+	public static double getEconBalance(String playerName) {
+		return econ.getBalance(playerName);
+	}
+	public static void withdrawFromPlayer(String playerName, double amount) {
+		econ.withdrawPlayer(playerName, amount);
+	}
+	public static void depositToPlayer(String playerName, double amount) {
+		econ.depositPlayer(playerName, amount);
+	}
+	
+	//access item namesToIds
+	public static boolean hasItem(String name) {
+		if (namesToIds.containsKey(name.toLowerCase())){
+			return true;
+		}
+		//check if the player's item name is an abbreviation
+		//we also must check that the abbreviation only matches one key
+		int numMatches = 0;
+		for (String key : namesToIds.keySet()) {
+			
+			if (key.startsWith(name.toLowerCase())) {
+				numMatches++;
+			}
+		}
+		if (numMatches == 1) {
+			return true;
+		}
+		return false;
+	}
+	
+    public static ItemStack nameToItemStack(String name, Integer amt) {
     	// Let's make life a bit easier on us:
     	name = name.toLowerCase();
     	if(name.contains(":")) {
@@ -183,8 +125,16 @@ public class SimpleEcoSigns extends JavaPlugin implements Listener {
     			ItemStack tmp = new ItemStack(Integer.valueOf(name.split(":")[0]), amt, Short.valueOf(name.split(":")[1]));
     			return tmp;
     		} catch (NumberFormatException e) {
-	    		if(!namesToIds.containsKey(name.split(":")[0]))
+	    		if(!namesToIds.containsKey(name.split(":")[0])) {
+	    			//first check if the player's item name is an abbreviation (this may be problematic)
+	    			for (String key : namesToIds.keySet()) {
+	    				if (key.startsWith(name)) {
+	    					return new ItemStack(Integer.valueOf(namesToIds.get(key).split(":")[0]), amt, Short.valueOf(namesToIds.get(key).split(":")[1]));
+	    				}
+	    			}
+	    			
 	    			return new ItemStack(Material.matchMaterial(name.split(":")[0]), amt, Short.valueOf(name.split(":")[1]));
+	    		}
 	    		else {
 	    			String s2 = name.split(":")[0];
 	    			String s3 = name.split(":")[1];
@@ -196,43 +146,23 @@ public class SimpleEcoSigns extends JavaPlugin implements Listener {
     			ItemStack tmp = new ItemStack(Integer.valueOf(name), amt);
     			return tmp;
     		} catch (NumberFormatException e) {
-    			if(!namesToIds.containsKey(name.toLowerCase()))
+    			if(!namesToIds.containsKey(name.toLowerCase())) {
+    				for (String key : namesToIds.keySet()) {
+	    				if (key.startsWith(name)) {
+	    					return new ItemStack(Integer.valueOf(namesToIds.get(key).split(":")[0]), amt, Short.valueOf(namesToIds.get(key).split(":")[1]));
+	    				}
+	    			}
+    				
     				return new ItemStack(Material.matchMaterial(name), amt);
-    			else
+    			}
+    			else {
     				return new ItemStack(Integer.valueOf(namesToIds.get(name).split(":")[0]), amt, Short.valueOf(namesToIds.get(name).split(":")[1]));
+    			}
     		}
     	}
     }
-
-    @EventHandler
-    public void blockPlace(BlockPlaceEvent event) {
-        if (event.getBlock() instanceof Sign) {
-            Sign sign = (Sign) event.getBlock().getState();
-            // Don't let the user make this an auth'd sign
-            if (sign.getLine(0).matches(ChatColor.DARK_BLUE + "(?i)\\[Buy\\]") || sign.getLine(0).matches(ChatColor.DARK_BLUE + "(?i)\\[Sell\\]")) {
-                event.setCancelled(true);
-                return;
-            }
-        }
-        if (event.getBlockAgainst().getState() instanceof Sign && blockIsValidEcoSign((Sign)event.getBlockAgainst().getState())) {
-        	event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void blockBreak(BlockBreakEvent event) {
-        if (!(event.getBlock().getState() instanceof Sign)) {
-            return;
-        }
-        Sign s = (Sign) event.getBlock().getState();
-        if (blockIsValidEcoSign(s)) {
-            if (!event.getPlayer().hasPermission("simpleecosigns.destroy")) {
-                event.setCancelled(true);
-            }
-        }
-    }
     
-    private boolean blockIsValidEcoSign(Sign s) {
+    public static boolean blockIsValidEcoSign(Sign s) {
     	try {
     		Integer.valueOf(s.getLine(1));
     		String s2 = s.getLine(2);
@@ -243,9 +173,18 @@ public class SimpleEcoSigns extends JavaPlugin implements Listener {
     		try {
     			Integer.valueOf(s3);
     		} catch (NumberFormatException e) {
-    			if(!namesToIds.containsKey(s3.toLowerCase()))
-    				if(Material.matchMaterial(s3) == null)
+    			if(!namesToIds.containsKey(s3.toLowerCase())) {
+    				boolean b = false;
+    				for (String key : namesToIds.keySet()) {
+	    				if (key.startsWith(s3)) {
+	    					b = true;
+	    				}
+	    			}
+    				
+    				if(Material.matchMaterial(s3) == null && !b) {
     					return false;
+    				}
+    			}
     		}
     		Double.valueOf(s.getLine(3).replace("$", ""));
     	} catch (Exception e) {
